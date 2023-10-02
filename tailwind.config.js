b216b8093b0c4498729298f344cb137c65ddd5b0
Amelss <ameleyka@gmail.com>
2023-10-02 12:04:11 +0100

    Adjusting tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        workSans: ["Work Sans"],
+        sora: ["Sora"],
+      },
+      colors: {
+        primaryMain: "#120B48",
+        primaryText: "#616163",
+        primaryHeadings: "#616163",
+      },
+    },
   },
   plugins: [],
 };
